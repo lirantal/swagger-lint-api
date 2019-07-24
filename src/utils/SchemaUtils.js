9c5cef022381a18d82f91c38ba8de291d3206c3d
Raymond Camden <raymond.camden@aexp.com>
2019-07-24 13:14:55 -0500

    feat($location): add mutli result
    Adds support for multiple results

--- src/utils/SchemaUtils.js
+++ src/utils/SchemaUtils.js
@@ -1,41 +1,8 @@
 'use strict'
 
-const debug = require('debug')('SchemaUtils')
 const _ = require('lodash')
 
 class SchemaUtils {
-  /*
-  static recursivelyFindKeyValueInObjects(validationSchema, inputSchema, path) {
-    if (inputSchema && typeof inputSchema === 'object') {
-      for (const [key, value] of Object.entries(inputSchema)) {
-        if (key === validationSchema.key) {
-          debug(`found a ${validationSchema.key} key`)
-
-          const ret = validationSchema.keyValidator(value)
-          if (ret) {
-            debug(`key ${validationSchema.key} is valid in path: ${path}`)
-          } else {
-            debug(`key ${validationSchema.key} is invalid in path: ${path}`)
-            return {
-              valid: false,
-              path: path.concat(key)
-            }
-          }
-        } else {
-          debug(`traversing deeper for key: ${validationSchema.key}`)
-          path.push(key)
-          const ret = SchemaUtils.recursivelyFindKeyValueInObjects(validationSchema, value, path)
-          if (ret) {
-            return ret
-          } else {
-            path.pop()
-          }
-        }
-      }
-    }
-  }
-  */
-
   static getPaths(obj, parentPath = [], paths = []) {
     if (typeof obj !== 'object') return paths
 
@@ -56,27 +23,6 @@ class SchemaUtils {
 
   static recursivelyFindKeyValueInObjects(validationSchema, inputSchema, path) {
     let results = []
-
-    /*
-    function getPaths(obj, parentPath = [], paths = []) {
-      if(typeof obj !== "object") return paths;
-
-      for(let prop in obj){
-          const currentPath = [...parentPath, prop];
-
-          paths.push([...currentPath]);
-
-          getPaths(obj[prop], currentPath, paths);
-      }
-
-      return paths;
-    }
-    */
-    /*
-    function findProps(obj, target) {
-      return getPaths(obj).filter(path => path.includes(target));
-    }
-    */
 
     const matches = this.findProps(inputSchema, 'description')
 
